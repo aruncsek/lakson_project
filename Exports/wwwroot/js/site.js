17091fa7a16a6f548b08767d7cd2587b0fd7a774
aruncsek <arunpandiancsek@gmail.com>
2024-09-12 17:46:40 +0530

    simple commit

---
+++ Exports/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+/*$(document).ready(function () {
+    $('#myTable').DataTable(
+        {
+            "processing": true,
+            "serverSide": true,
+            "filter": true,
+            "ajax": {
+                "url": "?handler=LoadData",
+                "type": "POST",
+                "datatype": "json"
+            },
+            "columns": [
+                { "data": "Model_Name"},                
+                { "data": "Id"}
+            ]
+        }
+    );
+});*/
